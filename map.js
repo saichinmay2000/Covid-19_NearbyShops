619b535d21de25ce4536fd6524b2800ca13d08ea
saichinmay2000 <saichinmayt@gmail.com>
2020-03-24 18:33:14 +0530

    Update map.js

--- map.js
+++ map.js
@@ -14,26 +14,23 @@ function showPosition(position) {
 }
 
 function store(){
- 
-
   firebase.auth().onAuthStateChanged(user => {
-var name = document.getElementById("name").value;
-var shpname = document.getElementById("shpname").value;
-var address = document.getElementById("address").value;
-var mobnum = document.getElementById("mobnum").value;
-var shoplicense = document.getElementById("shoplicense").value;
-var frontlicense = document.getElementById("frontlicense").value;
-var backlicense = document.getElementById("backlicense").value;
-var shoppic = document.getElementById("shoppic").value;
-var shopowner = document.getElementById("shopowner").value;
-var mail = user.email;
+  var name = document.getElementById("name").value;
+  var shpname = document.getElementById("shpname").value;
+  var address = document.getElementById("address").value;
+  var mobnum = document.getElementById("mobnum").value;
+  var shoplicense = document.getElementById("shoplicense").value;
+  var frontlicense = document.getElementById("frontlicense").value;
+  var backlicense = document.getElementById("backlicense").value;
+  var shoppic = document.getElementById("shoppic").value;
+  var shopowner = document.getElementById("shopowner").value;
+  var mail = user.email;
   firebase.database().ref(user.uid).child("Shop Details").set({
   Name: name,
   ShopName:shpname,
   Address: address,
   MobileNumber: mobnum,
   ShopLiscense:shoplicense
-
 });
 });
 }

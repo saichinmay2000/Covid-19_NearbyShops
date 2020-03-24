b4aaf71f52aef9448453a5d691566533b93dec1b
Lmas Lmas <30lmas09@gmail.com>
2020-03-24 19:01:26 +0530

    with image insertion

--- map.js
+++ map.js
@@ -12,6 +12,23 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
+var FrontImg,BackImg,ShopImg,OwnerImg;
+function Front(){
+  FrontImg = event.target.files[0];
+  console.log(FrontImg.name);
+}
+function Back(){
+  BackImg = event.target.files[0];
+  console.log(BackImg.name);
+}
+function Shop(){
+  ShopImg = event.target.files[0];
+  console.log(ShopImg.name);
+}
+function Owner(){
+  OwnerImg = event.target.files[0];
+  console.log(OwnerImg.name);
+}
 
 function store(){
  
@@ -22,11 +39,12 @@ var shpname = document.getElementById("shpname").value;
 var address = document.getElementById("address").value;
 var mobnum = document.getElementById("mobnum").value;
 var shoplicense = document.getElementById("shoplicense").value;
-var frontlicense = document.getElementById("frontlicense").value;
-var backlicense = document.getElementById("backlicense").value;
+var frontlicense = document.getElementById("frontlicense");
+var backlicense = document.getElementById("backlicense");
 var shoppic = document.getElementById("shoppic").value;
 var shopowner = document.getElementById("shopowner").value;
 var mail = user.email;
+
   firebase.database().ref(user.uid).child("Shop Details").set({
   Name: name,
   ShopName:shpname,
@@ -35,5 +53,52 @@ var mail = user.email;
   ShopLiscense:shoplicense
 
 });
+var storage = firebase.storage().ref('Images');
+var metadata = {
+  contentType: 'image/jpeg',
+};
+
+var uploadTask = storage.put(FrontImg,metadata);
+var uploadTask1 = storage.put(BackImg,metadata);
+var uploadTask2 = storage.put(ShopImg,metadata);
+var uploadTask3 = storage.put(OwnerImg,metadata);
+uploadTask.on('state_changed', function(snap){
+
+},function(error){
+
+},function(){
+  var downloadurl = uploadTask.snap.downloadURL;
+  console.log(downloadurl);
+  
+});
+uploadTask1.on('state_changed', function(snap){
+
+},function(error){
+
+},function(){
+  var downloadurl = uploadTask.snap.downloadURL;
+  console.log(downloadurl);
+  
+});
+uploadTask2.on('state_changed', function(snap){
+
+},function(error){
+
+},function(){
+  var downloadurl = uploadTask.snap.downloadURL;
+  console.log(downloadurl);
+  
+});
+uploadTask3.on('state_changed', function(snap){
+
+},function(error){
+
+},function(){
+  var downloadurl = uploadTask.snap.downloadURL;
+  console.log(downloadurl);
+  
+});
+
+  
 });
 }

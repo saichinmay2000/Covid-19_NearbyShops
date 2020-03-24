c9e707ba19e9de27701046b5ab9a23c179fe3f0e
Lmas Lmas <30lmas09@gmail.com>
2020-03-24 15:51:45 +0530

    with google firebase authenthication

---
+++ index.js
@@ -0,0 +1,14 @@
+(function(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyCrXaMutwiZ5wkQnZlN46XGnQpx1kVqhqM",
+    authDomain: "shopkeeper-4f9d9.firebaseapp.com",
+    databaseURL: "https://shopkeeper-4f9d9.firebaseio.com",
+    projectId: "shopkeeper-4f9d9",
+    storageBucket: "shopkeeper-4f9d9.appspot.com",
+    messagingSenderId: "555084841662",
+    appId: "1:555084841662:web:a29abe40ba32d98ad914ef",
+    measurementId: "G-YDD47BBRYH"
+};  
+firebase.initializeApp(firebaseConfig);  
+
+})()

5b8f61865d8c6c9d112a0e536b4706d9eb7e77f4
Sumanth <sumanthnani10@gmail.com>
2020-03-29 23:49:10 +0530

    map changed in cust

---
+++ CustomersPage/js/gmap.js
@@ -0,0 +1,46 @@
+var map, infoWindow,newress;
+function initMap() {
+	console.log('qwertyu');
+    map = new google.maps.Map(document.getElementById('sh_map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {lat: position.coords.latitude,lng: position.coords.longitude};
+        infoWindow.open(map);
+        map.setCenter(pos);
+        var marker = new google.maps.Marker({position:pos,draggable: false,map: map,title: 'Shop Location'});
+        console.log(pos);
+        firebase.auth().onAuthStateChanged(user => {
+        marker.addListener('position_changed', function(){
+            console.log(marker.getPosition().toString());
+            newres=marker.getPosition().toString();
+                 map.setCenter(marker.getPosition());
+                 var root= firebase.database().ref().child("ShopLocation").child(user.uid);
+                 root.set({
+                    ShopLocation:marker.getPosition().toString()
+                 });
+        });
+        });
+
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+            
+        });
+        
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());}
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+    
+}
+
+

38fe0b5728feaf09f063fc5712a05ea5c3ca8738
LMAS <60876387+lmas3009@users.noreply.github.com>
2020-04-10 17:16:18 +0530

    edit

--- js/user.js
+++ js/user.js
@@ -1,9 +1,12 @@
 
 (function(){
-  
     firebase.auth().onAuthStateChanged(user => {
         document.getElementById("email").innerHTML=user.email;
-        
+        /*firebase.database().ref("Name").child("name").set({
+          name:"Sumanth",
+          Items:"10",
+          Price:"7090"
+        })*/
         
         var root12 =  firebase.database().ref(user.uid).child("Shop Details");
         root12.once("value").then(function(snap){
@@ -34,7 +37,62 @@
 
       var root =  firebase.database();
       var root1 =  firebase.database().ref("Orders");
-      root1.once("value").then(function(snap){
+      root.ref("Name").child("name").once("value").then(function(snap){
+        var Name = snap.child("name").val();
+        var Itms = snap.child("Items").val();
+        var price = snap.child("Price").val();
+        var button = document.createElement('input');
+                      
+        // button.onclick=GetTableValues(childKey)
+      button.setAttribute('type', 'button');
+      button.setAttribute('value', 'Bill It');
+      button.setAttribute('onclick', 'Bill()');
+      button.setAttribute("class", "btn btn-info btn-xs");
+      button.setAttribute("data-toggle","modal");
+      button.setAttribute("data-target","#item_form");
+      if(Name!="" && Itms!=""){
+        var table  = document.getElementsByClassName("table1")[0];
+        var newrow = table.insertRow(1);
+        var cel1 = newrow.insertCell(0);
+        var cel2 = newrow.insertCell(1);
+        var cel3 = newrow.insertCell(2);
+        var res,res1;
+       
+        cel1.innerHTML=Name
+        cel2.innerHTML=Itms;
+        cel3.appendChild(button);
+      }
+      })
+      /*root.ref("Approval").once("value").then(function(snap){
+        snap.forEach(function(childSnapshot) {
+          var childKey = childSnapshot.key;
+          root.ref("Approval").child(childKey).once("value").then(function(snap1){
+            snap1.forEach(function(childSnapshot1) {
+              console.log(user.uid==childSnapshot1.key);
+              console.log(childSnapshot1.key);
+              
+              
+              if(user.uid==childSnapshot1.key){
+      root.ref("Approval").child(childKey).child(user.uid).once("value").then(function(snap){*/
+        
+        root.ref("Approval").child("name").once("value").then(function(snap){
+          var name1 = snap.child("name").val();
+          var itms1 = snap.child("Items").val();
+          var Pric = snap.child("Price").val();
+        var table  = document.getElementsByClassName("table2")[0];
+        var newrow = table.insertRow(1);
+        var cel1 = newrow.insertCell(0);
+        var cel2 = newrow.insertCell(1);
+        var cel3 = newrow.insertCell(2);
+       
+        cel1.innerHTML=name1;
+        cel2.innerHTML=itms1;
+        cel3.innerHTML = Pric;
+        
+      })
+    })
+})()
+     /* root1.once("value").then(function(snap){
           snap.forEach(function(childSnapshot) {
             var childKey = childSnapshot.key;
             
@@ -98,19 +156,22 @@
         var cel2 = newrow.insertCell(1);
         var cel3 = newrow.insertCell(2);
        
-        cel1.innerHTML=snap.child("Name").val();
-        cel2.innerHTML=snap.child("Items").val();
-        cel3.innerHTML = snap.child("Price").val();
+        cel1.innerHTML=Name;
+        cel2.innerHTML=Itms;
+        cel3.innerHTML = price;
         
       })
     }
     })
+    
+  })
   })
 
 })
       })
 });
     })()
+    */
 function Logout(){
     firebase.auth().signOut().then(function() {
   // Sign-out successful.
@@ -120,12 +181,16 @@ function Logout(){
   });
   }
 
-
-  function Bill(a){
+  function Bill(){
     var Name,Itms;
     var one,two,three,four,five,six,seven,eight,nine,qone,qtwo,qthree,qfour,qfive,qsix,qseven,qeight,qnine,qten;
     var root = firebase.database();
     firebase.auth().onAuthStateChanged(user => {
+      root.ref("Approval").child("name").set({
+        name:"Sumanth",
+        Items:"10",
+        Price:"7090"
+      })
     root.ref("Orders").child(a).child(user.uid).once("value").then(function(snap){
        Name = snap.child("Name").val();
        Itms = snap.child("Items").val();
@@ -169,8 +234,8 @@ function Logout(){
         document.getElementById("item_quantity_7").value=qseven;
         document.getElementById("item_quantity_8").value=qeight;
         document.getElementById("item_quantity_9").value=qnine;
-        document.getElementById("item_quantity_10").value=qten;*/
-      
+        document.getElementById("item_quantity_10").value=qten;
+      */
     root.ref("Approval").child(a).child(user.uid).set({
       Name:Name,
       Items:Itms,
@@ -182,5 +247,10 @@ function Logout(){
   })
     
   })
+ 
   }
+  function delete1(){
+    firebase.database().ref("Name").child("name").remove();
+  }
+  
   
